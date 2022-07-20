72c32bebd038b0ae29212fd827210ad3e7cd7ebd
Sushant <sushant.nalawade@yahoo.in>
2022-07-20 23:05:50 +0530

    first commit

---
+++ src/main/java/com/springcloudconfigserver/springcloudconfigserver/SpringcloudconfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.springcloudconfigserver.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudconfigserverApplication.class, args);
+	}
+
+}
